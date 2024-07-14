0db6ee62254b9ea8f93c03082f4fb70f1cea6068
Shiphan <140245703+Shiphan@users.noreply.github.com>
2024-07-14 22:43:03 +0800

    add layout

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,16 @@ fn handle_events() -> Result<bool> {
 }
 
 fn ui(frame: &mut Frame) {
+    let areas = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Length(1),
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ],
+    )
+    .split(frame.size());
+
     let current_dir = env::current_dir().unwrap();
     let read_dir = fs::read_dir(current_dir.clone()).unwrap();
     let items: Vec<String> = read_dir
@@ -50,12 +60,14 @@ fn ui(frame: &mut Frame) {
         .collect();
 
     let list = List::new(items)
-        .block(Block::bordered().title(current_dir.to_str().unwrap()))
+        // .block(Block::bordered().title(current_dir.to_str().unwrap()))
         .style(Style::default().fg(Color::White))
         .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
         .highlight_symbol(">>")
         .repeat_highlight_symbol(true)
         .direction(ListDirection::TopToBottom);
 
-    frame.render_widget(list, frame.size());
+    frame.render_widget(Paragraph::new(current_dir.to_str().unwrap()), areas[0]);
+    frame.render_widget(list, areas[1]);
+    frame.render_widget(Paragraph::new("end line here~~~"), areas[2]);
 }
